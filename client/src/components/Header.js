8fcf1533c672bc09491a59fd6dc2ff93d8b47feb
Karan <karan@karans-air.home>
2023-07-15 23:16:43 +0200

    redux-toolkit rendring

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,46 +9,58 @@ import {
   Tab,
 } from "@mui/material";
 import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 const Header = () => {
-  const [value, setValue] = useState(2);
+  const [value, setValue] = useState();
+  const isLogin = useSelector((state) => state.isLogin);
+  console.log(isLogin);
   return (
     <React.Fragment>
       <AppBar position="sticky">
         <Toolbar>
           <Typography variant="h4">My Blog App</Typography>
-          <Box display="flex" marginLeft="auto" marginRight="auto">
-            <Tabs
-              textColor="inherit"
-              value={value}
-              onChange={(e, val) => setValue(val)}
-            >
-              <Tab label="Blogs" LinkComponent={Link} to="/" />
-              <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
-            </Tabs>
-          </Box>
+          {isLogin && (
+            <Box display="flex" marginLeft="auto" marginRight="auto">
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => setValue(val)}
+              >
+                <Tab label="Blogs" LinkComponent={Link} to="/" />
+                <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
+              </Tabs>
+            </Box>
+          )}
           <Box display="flex" marginLeft="auto">
-            <Button
-              sx={{margin: 1, color: "white"}}
-              LinkComponent={Link}
-              to="/login"
-            >
-              Login
-            </Button>
-            <Button
-              sx={{margin: 1, color: "white"}}
-              LinkComponent={Link}
-              to="/register"
-            >
-              Register
-            </Button>
-            <Button
-              sx={{margin: 1, color: "white"}}
-              LinkComponent={Link}
-              to="/logout"
-            >
-              Logout
-            </Button>
+            {!isLogin && (
+              <>
+                <Button
+                  sx={{margin: 1, color: "white"}}
+                  LinkComponent={Link}
+                  to="/login"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{margin: 1, color: "white"}}
+                  LinkComponent={Link}
+                  to="/register"
+                >
+                  Register
+                </Button>
+              </>
+            )}
+
+            {isLogin && (
+              <Button
+                sx={{margin: 1, color: "white"}}
+                LinkComponent={Link}
+                to="/logout"
+              >
+                Logout
+              </Button>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Login = () => {
-  return <h1>Login Page</h1>;
+  return <h1>Login</h1>;
 };
 
 export default Login;
